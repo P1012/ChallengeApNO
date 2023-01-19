9b4cf4b5df13c7275e26159357f69475d1f4a639
Pawel Tatara <paweltatara45@gmail.com>
2023-01-19 11:08:04 +0100

    First code "Ewa lat 33 "

--- ChallengeAppNO/ChallengeAppNO/Program.cs
+++ ChallengeAppNO/ChallengeAppNO/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Var myDetails = Ewas ID
+
+string name = "Ewa";
+int age = 33;
+char sex= 'k';
+if (sex == 'k' && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
+else if (sex == 'm' && age == 39)
+{
+    Console.WriteLine("Cos o mnie");
+}
+
+
+
