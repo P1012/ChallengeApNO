417131e357f47fbf98f618bbc3054e732b82855e
Pawel Tatara <paweltatara45@gmail.com>
2023-01-20 20:52:10 +0100

    Do dokonczenia

---
+++ Numbers day nr. 5/Numbers day nr. 5/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+int number = 10044;
+
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray();
+
+List<char> digit  = new List<char>();
+
+digit.Add('0');
+digit.Add('1');
+digit.Add('2');
+digit.Add('3');
+digit.Add('4');
+digit.Add('5');
+digit.Add('6');
+digit.Add('7');
+digit.Add('8');
+digit.Add('9');
+
+foreach (var value in digit)
+{
+    int count = 0;
+    foreach (var x in letters)
+        if(value == x) 
+   
+    { count++; }
+
+    Console.WriteLine(value + "=>" + count);
+} 
+
+
+
+
+
