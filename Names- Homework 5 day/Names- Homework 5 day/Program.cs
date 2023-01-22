661c31c70f04166fc7086dbbda4daaa1b088c7ee
Pawel Tatara <paweltatara45@gmail.com>
2023-01-22 12:16:51 +0100

    Nie Udane

---
+++ Names- Homework 5 day/Names- Homework 5 day/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int[] grades= new int[20];
+
+List<string> fewNames = new List<string>();
+fewNames.Add("Pawel") ;
+fewNames.Add("Ryszard"); 
+fewNames.Add("Søren");
+fewNames.Add("Linda");
+fewNames.Add("Lars") ; 
+for (var f = 0; f< fewNames.Count; f++)
+{
+    Console.WriteLine(fewNames[f]);
+}
+for (int f = 0; f < 20; f = f+4)
+{
+    Console.WriteLine(f); 
+}
